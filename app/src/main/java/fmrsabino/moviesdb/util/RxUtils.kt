19f8667b1b33867f37e152b3252f0ca549b4439b
fmrsabino <fredericomsabino@gmail.com>
2017-06-17 20:31:56 +0200

    Update dependencies
    - Update gradle version
    - Update support lib version
    - Update Android Architecture Components version
    - Remove RxInterop
    - Google maven now declared with google()

--- app/src/main/java/fmrsabino/moviesdb/util/RxUtils.kt
+++ app/src/main/java/fmrsabino/moviesdb/util/RxUtils.kt
@@ -1,10 +1,8 @@
 package fmrsabino.moviesdb.util
 
-import hu.akarnokd.rxjava.interop.RxJavaInterop
 import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
-fun <T> rx.Observable<T>.toRx2(): Observable<T> = RxJavaInterop.toV2Observable(this)
 
 fun <T> Observable<T>.debounceExceptFirst(timeout: Long, unit: TimeUnit) =
-    this.publish { items -> items.take(1).concatWith(items.skip(1).debounce(timeout, unit)) }
+        this.publish { items -> items.take(1).concatWith(items.skip(1).debounce(timeout, unit)) }
